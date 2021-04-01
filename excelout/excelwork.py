5dedc6c88dca2414f622df0b13969794084ea2ee
AE@example.com <student@bchd.6536dda7-44a6-43d5-8344-b1e787b0148d>
2021-04-01 16:21:28 +0000

    saving my excel file

--- excelout/excelwork.py
+++ excelout/excelwork.py
@@ -10,7 +10,9 @@ import pyexcel
 def get_ip_data():
     input_ip = input("\nWhat is the IP address? ")
     input_driver = input("What is the driver associated with this device? ")
-    d = {"IP": input_ip, "driver": input_driver}
+    input_port = input("What is the port you want ot use?")
+    input_priority= input("What is the priority for this entry")
+    d = {"IP": input_ip, "driver": input_driver , "port":input_port, "priority":input_priority}
     return d
 
 ## This code is left turned off, but might help visualize how pyexcel works with data sets.
